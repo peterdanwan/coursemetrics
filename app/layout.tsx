5c619c3eb359cb23c5468aded5c5f6e86397863a
peterdanwan <peterdanwan@gmail.com>
2024-09-01 23:32:57 -0400

    added initial implementation of footer. added a new colour called 'ourBG' used in Footer and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 // app\layout.tsx
-import type { Metadata } from "next";
-import NavBar from "@/components/Navbar/Navbar";
-import "./globals.css";
-import { Providers } from "./providers";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import type { Metadata } from 'next';
+import NavBar from '@/components/Navbar/Navbar';
+import Footer from '@/components/Footer/Footer';
+import './globals.css';
+import { Providers } from './providers';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export const metadata: Metadata = {
-  title: "CourseMetrics",
-  description: "PRJ666 Final Project",
+  title: 'CourseMetrics',
+  description: 'PRJ666 Final Project',
 };
 
 export default function RootLayout({
@@ -16,12 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <UserProvider>
-        <body className="bg-[#0B2027] text-white min-h-screen">
+        <body className='bg-ourBG text-white min-h-screen'>
           <Providers>
             <NavBar />
             {children}
+            <Footer />
           </Providers>
         </body>
       </UserProvider>
