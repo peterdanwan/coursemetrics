2c5d8190e5c93cf85e5548801d4dc37b6e2b73fa
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-14 11:45:48 -0400

    Make sure the responses returned from the API have a standard response

---
+++ utils/response.ts
@@ -0,0 +1,40 @@
+// utils/response.ts
+
+import { type ISuccessResponse, type IErrorResponse } from "@/interfaces";
+
+/**
+ * A successful response looks like:
+ *
+ * {
+ *   "status": "ok",
+ *   ...
+ * }
+ */
+
+export const createSuccessResponse = function <T>(
+  data: T,
+): ISuccessResponse<T> {
+  return {
+    status: "ok",
+    data,
+  };
+};
+
+/**
+ * An error response looks like:
+ *
+ * {
+ *   "status": "error",
+ *   "error": {
+ *     "code": 400,
+ *     "message": "invalid request, missing ...",
+ *   }
+ * }
+ */
+
+export const createErrorResponse = function (
+  code: number,
+  message: string,
+): IErrorResponse {
+  return { status: "error", error: { code: code, message: message } };
+};
