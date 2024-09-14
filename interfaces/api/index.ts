d93000c4235efa619dd2580dc88e27960a46b4c8
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-14 11:47:16 -0400

    Specify interfaces for type safety

---
+++ interfaces/api/index.ts
@@ -0,0 +1,3 @@
+// interfaces/api/index.ts
+
+// This is a placeholder for all interfaces we might need for API routes since there are many of them
---
+++ interfaces/pages/index.ts
@@ -0,0 +1,3 @@
+// interfaces/pages/index.ts
+
+// This is a placeholder for all interfaces we might need for pages since there are many of them
