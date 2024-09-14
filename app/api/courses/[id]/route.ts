7d78d944853b6d50d2ae25d381a9c1c599e4688f
Peter Wan <peterdanwan@gmail.com>
2024-09-14 15:07:00 -0400

    Removed unnecessary if (!course) check
    Co-authored-by: Aryan Khurana <AryanK1511@users.noreply.github.com>

--- app/api/courses/[id]/route.ts
+++ app/api/courses/[id]/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse, NextRequest } from "next/server";
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
-import connectDB from "@/config/database";
-import { createSuccessResponse, createErrorResponse } from "@/utils";
-import { Course } from "@/models/Course";
-import { logger } from "@/utils";
+import { NextResponse, NextRequest } from 'next/server';
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+import connectDB from '@/config/database';
+import { createSuccessResponse, createErrorResponse } from '@/utils';
+import { Course } from '@/models/Course';
+import { logger } from '@/utils';
 
 // ===== API ROUTE TO FETCH A COURSE BY ID =====
 
 export const GET = withApiAuthRequired(async function get_course_by_id( // Ignore this warning. It is some typescript issue that we can fix later
   req: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: { id: string } }
 ): Promise<NextResponse> {
-  const log = logger.child({ module: "api/courses/[id]" });
+  const log = logger.child({ module: 'api/courses/[id]' });
 
   try {
     await connectDB();
@@ -19,42 +19,34 @@ export const GET = withApiAuthRequired(async function get_course_by_id( // Ignor
 
     // Validate course ID
     if (!courseId) {
-      log.error("Course ID is required");
+      log.error('Course ID is required');
       return NextResponse.json(
-        createErrorResponse(400, "Course ID is required"),
-        { status: 400 },
+        createErrorResponse(400, 'Course ID is required'),
+        { status: 400 }
       );
     }
 
-    log.info("Fetching course", { courseId });
+    log.info('Fetching course', { courseId });
 
     // Fetch the course by ID
     const course = await Course.findById(courseId)
-      .populate("courseDetailId")
+      .populate('courseDetailId')
       .exec();
 
-    // Check if the course exists
-    if (!course) {
-      log.error("Course not found", { courseId });
-      return NextResponse.json(createErrorResponse(404, "Course not found"), {
-        status: 404,
-      });
-    }
-
-    log.debug({ course }, "Course fetched from DB");
-    log.info("Course fetched successfully", { courseId });
+    log.debug({ course }, 'Course fetched from DB');
+    log.info('Course fetched successfully', { courseId });
 
     return NextResponse.json(createSuccessResponse({ course }), {
       status: 200,
     });
   } catch (error: unknown) {
     console.error(error);
-    log.error("Error fetching course", { error });
+    log.error('Error fetching course', { error });
 
     if (error instanceof Error) {
-      if (error.name === "CastError") {
-        log.error("Course not found");
-        return NextResponse.json(createErrorResponse(404, "Course not found"), {
+      if (error.name === 'CastError') {
+        log.error('Course not found');
+        return NextResponse.json(createErrorResponse(404, 'Course not found'), {
           status: 404,
         });
       }
@@ -66,9 +58,9 @@ export const GET = withApiAuthRequired(async function get_course_by_id( // Ignor
     return NextResponse.json(
       createErrorResponse(
         500,
-        "Something went wrong. A server-side issue occurred.",
+        'Something went wrong. A server-side issue occurred.'
       ),
-      { status: 500 },
+      { status: 500 }
     );
   }
 });
