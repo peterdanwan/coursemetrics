8d5a8882130ad27206c018241e3b45116b2e358e
Aryan Khurana <aryankhurana1511@gmail.com>
2024-09-19 23:54:31 -0400

    Remove loading file

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Spinner } from '@nextui-org/react';
-
-// Ref Doc: https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
-export default function Loading() {
-  return <Spinner />;
-}
