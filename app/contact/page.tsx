b1ee3e345c1a4d3ef09ddb51d1e9fb7ce9e46f97
Peter Wan <97978729+peterdanwan@users.noreply.github.com>
2024-10-02 00:55:35 -0400

    fix(contact): change `'` into `&apos`;

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,7 +35,7 @@ export default function Contact() {
             Contact Us
           </Heading>
           <Text fontSize="lg" mt={2}>
-            We're here to help!
+            We&apos;re here to help!
           </Text>
           <Text mt={4}>
             If you have any questions, concerns, or feedback, please don&apos;t hesitate to reach
