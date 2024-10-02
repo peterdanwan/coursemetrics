b244afdbef2cf3f507c4cacc4103d6525458dc93
mdang10 <mdang10@myseneca.ca>
2024-10-01 22:11:21 -0400

    Complete the Contact Us Page with a customer service image

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,12 @@
 // app/contact/page.tsx
 'use client';
 import { Box, Flex, Text, Heading, Link } from '@chakra-ui/react';
-import React from 'react';
+import { EmailIcon, PhoneIcon, InfoIcon } from '@chakra-ui/icons';
 import Image from 'next/image';
-import logo from '@/assets/images/CourseMetricsLogo.png';
+import React from 'react';
+import contactIllustration from '@/assets/images/customerService.jpg';
 
-export default function About() {
+export default function Contact() {
   return (
     <Flex
       direction={{ base: 'column', md: 'row' }}
@@ -13,6 +14,7 @@ export default function About() {
       justify="center"
       p={5}
       minHeight="100vh"
+      bg="gray.50" // use "gray.50" for light version and "bg-ourBG" for dark version
     >
       <Box
         flex={{ base: '1', md: '0 1 40%' }}
@@ -22,21 +24,83 @@ export default function About() {
         display="flex"
         alignItems="center"
         justifyContent="center"
+        p={8}
+        borderRadius="lg"
+        shadow="md"
+        bg="white"
+        color="black"
       >
         <Box>
-          <Heading as="h1" size="2xl">
+          <Heading as="h1" size="2xl" mb={4}>
             Contact Us
           </Heading>
-          <Text fontSize="lg" mt={12}>
-            Have an issue?
+          <Text fontSize="lg" mt={2}>
+            We’re here to help!
           </Text>
           <Text mt={4}>
-            Please send us an email at{' '}
+            If you have any questions, concerns, or feedback, please don't hesitate to reach out to
+            us. Your input is invaluable to us.
+          </Text>
+          <Text mt={4}>
+            Email us at{' '}
             <Link href="mailto:coursemetrics@helpdesk.com" color="teal.500">
               coursemetrics@helpdesk.com
-            </Link>{' '}
-            or fill out the form with your issue or feedback.
+            </Link>
+            . We respond to inquiries within <strong>24 hours</strong>!
           </Text>
+          <Flex alignItems="center" mt={4}>
+            <EmailIcon boxSize={5} color="teal.500" />
+            <Text ml={2}>
+              Email:{' '}
+              <Link href="mailto:coursemetrics@helpdesk.com" color="teal.500">
+                coursemetrics@helpdesk.com
+              </Link>
+            </Text>
+          </Flex>
+          <Flex alignItems="center" mt={2}>
+            <PhoneIcon boxSize={5} color="teal.500" />
+            <Text ml={2}>Phone: (123) 456-7890</Text>
+          </Flex>
+          <Text mt={4}>Customer Support Hours: 9 AM - 5 PM (Mon - Fri)</Text>
+          <Text mt={4}>Common inquiries include:</Text>
+          <Text mt={2} pl={4}>
+            • Account issues <br />
+            • Technical support <br />
+            • Feedback on our services <br />• General questions
+          </Text>
+          <Text mt={4}>
+            We value your feedback! Let us know how we can improve your experience with
+            CourseMetrics.
+          </Text>
+          <Text mt={4}>Connect with us on social media:</Text>
+          <Flex direction="column" mt={2}>
+            <Flex alignItems="center">
+              <InfoIcon boxSize={5} color="teal.500" />
+              <Link href="https://twitter.com/yourprofile" color="teal.500" ml={2}>
+                Twitter
+              </Link>
+            </Flex>
+            <Flex alignItems="center" mt={1}>
+              <InfoIcon boxSize={5} color="teal.500" />
+              <Link href="https://facebook.com/yourprofile" color="teal.500" ml={2}>
+                Facebook
+              </Link>
+            </Flex>
+            <Flex alignItems="center" mt={1}>
+              <InfoIcon boxSize={5} color="teal.500" />
+              <Link href="https://instagram.com/yourprofile" color="teal.500" ml={2}>
+                Instagram
+              </Link>
+            </Flex>
+          </Flex>
+          <Text mt={4}>
+            Check our GitHub for more info or to report issues:
+            <Link href="https://github.com/yourusername/your-repo" color="teal.500">
+              {' '}
+              GitHub Repository
+            </Link>
+          </Text>
+          <Text mt={4}>Your feedback helps us serve you better!</Text>
         </Box>
       </Box>
       <Box
@@ -44,9 +108,15 @@ export default function About() {
         display="flex"
         alignItems="center"
         justifyContent="center"
+        mt={{ base: 5, md: 0 }}
       >
-        {/* Need another image and not the logo */}
-        <Image src={logo} alt="CourseMetrics Logo" width={300} height={300} />
+        <Image
+          src={contactIllustration}
+          alt="Contact Us Illustration"
+          width={700}
+          height={700}
+          style={{ borderRadius: '8px' }}
+        />
       </Box>
     </Flex>
   );
