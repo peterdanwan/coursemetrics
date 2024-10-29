ae7ce8de73d1f316eb4080003cb0a2da26a64040
trochwerger <tomasrochwerger3@gmail.com>
2024-10-28 21:21:05 -0400

    Added link for courseCard if header to show course without sending year and semester params

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -13,6 +13,7 @@ import {
   Select,
 } from '@chakra-ui/react';
 import { FaHeart } from 'react-icons/fa';
+import Link from 'next/link';
 
 interface ICourseTerm {
   season: string;
@@ -55,48 +56,52 @@ export default function CourseCard({ courses }: CourseCardProps) {
   };
 
   return (
-    <Card>
-      <CardHeader p={{ base: '3', sm: '3', md: '3' }}>
-        <Flex align="center" gap={2} wrap="wrap">
-          <Box>
-            <Heading
-              as="h1"
-              color="teal"
-              fontSize={{ base: '20', sm: '24', md: '24', lg: '28' }}
-              mb={2}
-            >
-              {selectedCourse.course_code}
-            </Heading>
-            <Heading as="h2" color="teal" fontSize={{ md: '18' }}>
-              {selectedCourse.CourseDetail.course_name}
-            </Heading>
-          </Box>
-          <Spacer />
-          <Box color="pink.400">
-            <FaHeart size={20} />
-          </Box>
-          <Box>
-            <Select
-              placeholder="Select Term"
-              size="sm"
-              onChange={handleTermChange}
-              defaultValue={selectedCourse.course_term_id}
-            >
-              {courses.map((course) => (
-                <option key={course.course_id} value={course.course_term_id}>
-                  {course.CourseTerm.season} {course.CourseTerm.year}
-                </option>
-              ))}
-            </Select>
-          </Box>
-        </Flex>
-      </CardHeader>
-      <CardBody p={{ base: '3', sm: '3', md: '3' }}>
-        <Text fontSize={{ md: '14' }}>{selectedCourse.CourseDetail.course_description}</Text>
-      </CardBody>
-      <Button colorScheme="teal" variant="outline" size="sm" onClick={navigateToCourse} mt={2}>
-        View Reviews
-      </Button>
-    </Card>
+    <>
+      <Card>
+        <CardHeader p={{ base: '3', sm: '3', md: '3' }}>
+          <Flex align="center" gap={2} wrap="wrap">
+            <Box>
+              <Link href={`/courses/${selectedCourse.course_code}`}>
+                <Heading
+                  as="h1"
+                  color="teal"
+                  fontSize={{ base: '20', sm: '24', md: '24', lg: '28' }}
+                  mb={2}
+                >
+                  {selectedCourse.course_code}
+                </Heading>
+              </Link>
+              <Heading as="h2" color="teal" fontSize={{ md: '18' }}>
+                {selectedCourse.CourseDetail.course_name}
+              </Heading>
+            </Box>
+            <Spacer />
+            <Box color="pink.400">
+              <FaHeart size={20} />
+            </Box>
+            <Box>
+              <Select
+                placeholder="Select Term"
+                size="sm"
+                onChange={handleTermChange}
+                defaultValue={selectedCourse.course_term_id}
+              >
+                {courses.map((course) => (
+                  <option key={course.course_id} value={course.course_term_id}>
+                    {course.CourseTerm.season} {course.CourseTerm.year}
+                  </option>
+                ))}
+              </Select>
+            </Box>
+          </Flex>
+        </CardHeader>
+        <CardBody p={{ base: '3', sm: '3', md: '3' }}>
+          <Text fontSize={{ md: '14' }}>{selectedCourse.CourseDetail.course_description}</Text>
+        </CardBody>
+        <Button colorScheme="teal" variant="outline" size="sm" onClick={navigateToCourse} mt={2}>
+          View Reviews
+        </Button>
+      </Card>
+    </>
   );
 }
