3ad3a7eeb2cd178f24c990446e6d06f80be83807
peterdanwan <peterdanwan@gmail.com>
2024-10-29 23:47:49 -0400

    fix: remove page and limit from courses and professors API routes

--- app/api/professors/route.ts
+++ app/api/professors/route.ts
@@ -26,51 +26,54 @@ export const GET = async function get_professors(req: NextRequest): Promise<Next
     };
 
     // Ref Doc: https://www.shecodes.io/athena/60744-what-is-parseint-in-javascript#
-    const pageNumber: number = parseInt(queryParams.page, 10);
-    const limitNumber: number = parseInt(queryParams.limit, 10);
+    // const pageNumber: number = parseInt(queryParams.page, 10);
+    // const limitNumber: number = parseInt(queryParams.limit, 10);
 
     // Validate pagination parameters
-    if (isNaN(pageNumber) || isNaN(limitNumber) || pageNumber < 1 || limitNumber < 1) {
-      log.error('Invalid pagination parameters', { pageNumber, limitNumber });
-      return NextResponse.json(createErrorResponse(400, 'Invalid pagination parameters'), {
-        status: 400,
-      });
-    }
+    // if (isNaN(pageNumber) || isNaN(limitNumber) || pageNumber < 1 || limitNumber < 1) {
+    //   log.error('Invalid pagination parameters', { pageNumber, limitNumber });
+    //   return NextResponse.json(createErrorResponse(400, 'Invalid pagination parameters'), {
+    //     status: 400,
+    //   });
+    // }
 
     // Calculate the number of documents to skip
-    const offsetNumber: number = (pageNumber - 1) * limitNumber;
+    // const offsetNumber: number = (pageNumber - 1) * limitNumber;
 
-    log.info('Fetching professors', { pageNumber, limitNumber });
+    // log.info('Fetching professors', { pageNumber, limitNumber });
 
     // Fetch the professors and count the total number of records
-    const professors = await Professor.findAll({ limit: limitNumber, offset: offsetNumber });
+    const professors = await Professor.findAll({
+      // limit: limitNumber,
+      // offset: offsetNumber
+    });
 
     console.log(professors);
 
     const totalProfessors: number = await Professor.count();
-    const totalPages: number = Math.ceil(totalProfessors / limitNumber);
+    // const totalPages: number = Math.ceil(totalProfessors / limitNumber);
 
     log.debug(
       {
         professors,
-        totalPages,
-        currentPage: pageNumber,
+        // totalPages,
+        // currentPage: pageNumber,
         totalProfessors,
       },
       'Professors fetched from DB'
     );
 
     log.info('Professors fetched successfully', {
-      totalPages,
-      currentPage: pageNumber,
+      // totalPages,
+      // currentPage: pageNumber,
       totalProfessors,
     });
 
     return NextResponse.json(
       createSuccessResponse({
         professors,
-        totalPages,
-        currentPage: pageNumber,
+        // totalPages,
+        // currentPage: pageNumber,
         totalProfessors,
       }),
       { status: 200 }
