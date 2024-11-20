7109280187aa9647c79c2e98a1efce841a4a113d
Aryan Khurana <aryankhurana1511@gmail.com>
2024-11-20 05:33:33 -0500

    feat: add ability to swap between development and production databases

--- database/sequelizeInstance.ts
+++ database/sequelizeInstance.ts
@@ -3,15 +3,33 @@
 import { Sequelize } from 'sequelize';
 import pg from 'pg'; // Ref: https://node-postgres.com
 
+// Local DB Setup
+// const sequelizeInstance = new Sequelize({
+//   host: process.env.POSTGRES_HOST,
+//   username: process.env.POSTGRES_USERNAME,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DATABASE,
+//   dialect: 'postgres',
+//   port: 5433,
+//   dialectModule: pg,
+//   benchmark: true,
+// });
+
+// Production DB Setup
 const sequelizeInstance = new Sequelize({
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'coursemetricsDB',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   dialect: 'postgres',
-  port: 5433,
   dialectModule: pg,
   benchmark: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export { sequelizeInstance };
