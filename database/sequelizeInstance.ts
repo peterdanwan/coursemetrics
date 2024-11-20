329cf4e77ca2f701d7b09dfd92c091b4bb948743
Aryan Khurana <aryankhurana1511@gmail.com>
2024-11-20 06:03:59 -0500

    chore: refactor code to make it env-var config style compliant

--- database/sequelizeInstance.ts
+++ database/sequelizeInstance.ts
@@ -3,33 +3,40 @@
 import { Sequelize } from 'sequelize';
 import pg from 'pg'; // Ref: https://node-postgres.com
 
-// Local DB Setup
-// const sequelizeInstance = new Sequelize({
-//   host: process.env.POSTGRES_HOST,
-//   username: process.env.POSTGRES_USERNAME,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DATABASE,
-//   dialect: 'postgres',
-//   port: 5433,
-//   dialectModule: pg,
-//   benchmark: true,
-// });
+let config: any = null;
+
+// local DB setup
+if (process.env.POSTGRES_HOST === 'localhost') {
+  config = {
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    dialect: 'postgres',
+    port: 5433,
+    dialectModule: pg,
+    benchmark: true,
+  };
+} else {
+  // Production DB Setup
+  config = {
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    dialect: 'postgres',
+    dialectModule: pg,
+    benchmark: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+}
 
 // Production DB Setup
-const sequelizeInstance = new Sequelize({
-  host: process.env.POSTGRES_HOST,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  dialect: 'postgres',
-  dialectModule: pg,
-  benchmark: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelizeInstance = new Sequelize(config);
 
 export { sequelizeInstance };
