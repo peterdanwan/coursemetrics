678a509a56728c3b1ffff2a9f1dd8f88fd9da873
mdang10 <mdang10@myseneca.ca>
2024-11-26 17:11:47 -0500

    Add Array.isArray for reviewData in Admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -39,8 +39,8 @@ export default withAdminAuth(function Admin({ user }: { user: any }) {
   const numberOfProfessors = professorData?.data.totalProfessors || 0;
   const numberOfCourses = courseData?.data.totalCourses || 0;
   const numberOfPendingReviews = Array.isArray(reviewData?.data)
-  ? reviewData?.data.filter((review: any) => review.review_status_id === 1).length
-  : 0;
+    ? reviewData?.data.filter((review: any) => review.review_status_id === 1).length
+    : 0;
 
   // Loading Spinner
   if (isLoading) {
